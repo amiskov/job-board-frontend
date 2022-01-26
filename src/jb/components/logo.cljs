2c12fa321ae59dcd64af779895ccdb9c07c8aea1
Andrey Miskov <amiskov@gmail.com>
2022-01-26 12:10:20 +0300

    Fix logo url

--- src/jb/components/logo.cljs
+++ src/jb/components/logo.cljs
@@ -2,7 +2,7 @@
 
 (defn logo []
   [:a.logo {:href "/"}
-   [:img.logo__img {:src "/img/logo.svg"}]
+   [:img.logo__img {:src "img/logo.svg"}]
    [:div.logo__text
     [:div.logo__text-top "Functional"]
     [:div.logo__text-bottom "Works"]]])
